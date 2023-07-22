8f3b9923851b89fbd322a952c24631fb3b59903f
Seonhub1 <131983341+Seonhub1@users.noreply.github.com>
2023-07-22 11:26:21 +0700

    tiktodv2.py

--- tiktodv2.py
+++ tiktodv2.py
@@ -101,8 +101,8 @@ def loop3():
     loop3()
 """
 
-vidUrl = "YOUR_URL" #Change YOUR_URL to your Tik Tok video URL
-username = "YOUR_USERNAME" #Change YOUR_USERNAME to your Tik Tok username
+vidUrl = "https://www.tiktok.com/t/ZSLmTm9hw/" #Change YOUR_URL to your Tik Tok video URL
+username = "KARUMASHOP" #Change YOUR_USERNAME to your Tik Tok username
 
 system("cls")
 tiktod = pyfiglet.figlet_format("TIKTOD V2", font="slant")
